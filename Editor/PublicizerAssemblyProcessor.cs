9686dce405108774617b94e8eb035a52e5d03f85
Nebby <nebby131999@gmail.com>
2022-04-17 18:00:27 -0400

    Re-Implemented priority for processors, finished optional executor

--- Editor/PublicizerAssemblyProcessor.cs
+++ Editor/PublicizerAssemblyProcessor.cs
@@ -1,14 +1,26 @@
-﻿using ThunderKit.Core.Config;
+﻿using System.IO;
+using ThunderKit.Core.Config;
+using UObject = UnityEngine.Object;
 
 namespace RiskOfThunder.RoR2Importer
 {
     public class PublicizerAssemblyProcessor : AssemblyProcessor
     {
-        public override int Priority => -10;
+        public override int Priority => 500;
         public override string Name => $"Assembly Publicizer";
 
         public override string Process(string path)
         {
+            PublicizerDataStorer dataStorer = PublicizerDataStorer.GetDataStorer();
+            //Publicizer not enabled? dont publicize
+            if(!dataStorer.enabled)
+                return path;
+
+            var assemblyFileName = Path.GetFileName(path);
+            //assembly file name is not in assemblyNames? dont publicize.
+            if (!dataStorer.assemblyNames.Contains(assemblyFileName))
+                return path;
+
             return path;
         }
     }
