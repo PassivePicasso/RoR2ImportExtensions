ae741e8f8501063697b4dc4c22136bb2e7cd24be
Nebby <nebby131999@gmail.com>
2022-04-17 16:36:42 -0400

    PublicizerDataStorer and AssemblyProcessor

---
+++ Editor/PublicizerAssemblyProcessor.cs
@@ -0,0 +1,15 @@
+﻿using ThunderKit.Core.Config;
+
+namespace RiskOfThunder.RoR2Importer
+{
+    public class PublicizerAssemblyProcessor : AssemblyProcessor
+    {
+        public override int Priority => -10;
+        public override string Name => $"Assembly Publicizer";
+
+        public override string Process(string path)
+        {
+            return path;
+        }
+    }
+}
---
+++ Editor/PublicizerDataStorer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ThunderKit.Core.Config;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace RiskOfThunder.RoR2Importer
+{
+    public class PublicizerDataStorer : OptionalExecutor
+    {
+        public override string Name => "Assembly Publicizer";
+        public override string Description => "Assemblies listed in here will be publicized using NStrip." +
+            "\nPublicized assemblies retain their inspector look and functionality, this does not strip assemblies.";
+        public override int Priority => ThunderKit.Common.Constants.ConfigPriority.AssemblyImport + 125_000;
+
+        public List<string> assemblyNames = new List<string> { "RoR2.dll" };
+
+        public override void Execute()
+        { }
+
+        protected override VisualElement CreateProperties()
+        {
+            SerializedObject obj = new SerializedObject(this);
+            PropertyField propField = new PropertyField(obj.FindProperty(nameof(assemblyNames)));
+            return propField;
+        }
+    }
+}
