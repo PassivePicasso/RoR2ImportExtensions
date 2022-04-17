5459244130132c6e255539d9766d5d77b61a138e
Nebby <nebby131999@gmail.com>
2022-04-17 19:19:40 -0400

    Hopefully finished publicizer assembly processor

--- Editor/PublicizerAssemblyProcessor.cs
+++ Editor/PublicizerAssemblyProcessor.cs
@@ -1,6 +1,15 @@
 ﻿using System.IO;
 using ThunderKit.Core.Config;
+using UnityEngine;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 using UObject = UnityEngine.Object;
+using System.Threading.Tasks;
+using UnityEditor;
+using System.Collections.Generic;
+using ThunderKit.Core.Data;
+using ThunderKit.Core.Paths;
+using System.Text;
 
 namespace RiskOfThunder.RoR2Importer
 {
@@ -8,20 +17,78 @@ namespace RiskOfThunder.RoR2Importer
     {
         public override int Priority => 500;
         public override string Name => $"Assembly Publicizer";
-
-        public override string Process(string path)
+        public override string Process(string assemblyPath)
         {
             PublicizerDataStorer dataStorer = PublicizerDataStorer.GetDataStorer();
             //Publicizer not enabled? dont publicize
             if(!dataStorer.enabled)
-                return path;
+                return assemblyPath;
 
-            var assemblyFileName = Path.GetFileName(path);
+            var assemblyFileName = Path.GetFileName(assemblyPath);
             //assembly file name is not in assemblyNames? dont publicize.
             if (!dataStorer.assemblyNames.Contains(assemblyFileName))
-                return path;
+                return assemblyPath;
 
-            return path;
+            UObject nstripExe = dataStorer.nStripExecutable;
+            if (nstripExe == null)
+            {
+                Debug.LogWarning($"Could not strip assembly {assemblyFileName}, as NStrip has not been located.");
+                return assemblyPath;
+            }
+
+            string ror2ManagedDir = ThunderKitSetting.GetOrCreateSettings<ThunderKitSettings>().ManagedAssembliesPath;
+
+            string thunderkitRoot = Application.dataPath.Replace("Assets", "ThunderKit");
+            string nstripFolder = Path.Combine(thunderkitRoot, "NStrip");
+            if(!Directory.Exists(nstripFolder))
+            {
+                Directory.CreateDirectory(nstripFolder);
+            }
+            string outputPath = Path.Combine(nstripFolder, assemblyFileName);
+            string nstripPath = Path.GetFullPath(AssetDatabase.GetAssetPath(nstripExe));
+
+            List<string> arguments = new List<string>
+            {
+                "NStrip",
+                "-p",
+                "-n",
+                $"-d \"{ror2ManagedDir}\"",
+                "-cg",
+                "-cg-exclude-events",
+                "-remove-readonly",
+                "-unity-non-serialized",
+                $"\"{assemblyPath}\"",
+                $"\"{outputPath}\""
+            };
+
+            List<string> log = new List<string> { $"Publicizedd {assemblyFileName} with the following arguments:" };
+            log.AddRange(StripAssembly(arguments, nstripPath));
+
+            return outputPath;
         }
+
+        private List<string> StripAssembly(List<string> arguments, string nstripPath)
+        {
+            var args = new StringBuilder();
+            var logger = new List<string>();
+            for (int i = 0; i < arguments.Count; i++)
+            {
+                args.Append(arguments[i]);
+                args.Append(" ");
+                logger.Add($"Argument {i}: '{arguments[i]}'");
+            }
+
+            ProcessStartInfo psi = new ProcessStartInfo(nstripPath)
+            {
+                WorkingDirectory = Path.GetDirectoryName(nstripPath),
+                Arguments = args.ToString(),
+                UseShellExecute = true
+            };
+            var process = System.Diagnostics.Process.Start(psi);
+            process.WaitForExit();
+            return logger;
+        }
+
+
     }
 }
