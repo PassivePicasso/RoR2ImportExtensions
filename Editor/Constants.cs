10ad313872b979a56112f2ec24ce8b01534ac3df
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-04-19 21:07:16 -0400

    Add constants class with priority class to consolidate priorities

---
+++ Editor/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace RiskOfThunder.RoR2Importer
+{
+    using static ThunderKit.Common.Constants;
+    public static class Constants
+    {
+        public static class Priority
+        {
+            public const int UnityPackageInstaller = ConfigPriority.AssemblyImport + 250_000;
+            public const int ThunderstorePackageInstaller = ConfigPriority.AddressableCatalog - 250_000;
+            public const int LegacyResourceAPIPatcher = ConfigPriority.AssemblyImport - 250_000;
+            public const int AssemblyPublicizerConfiguration = ConfigPriority.AssemblyImport + 125_000;
+        }
+    }
+}
