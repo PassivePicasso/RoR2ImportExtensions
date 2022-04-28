0ab23806bb00c914ad0930d1720c5d71e666b903
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-04-27 20:15:34 -0400

    Assign Screen space shadows shader

--- Editor/ConfigureAddressableGraphics.cs
+++ Editor/ConfigureAddressableGraphics.cs
@@ -9,5 +9,6 @@ namespace RiskOfThunder.RoR2Importer
         public override string Description => "Assigns the Risk of Rain 2 DeferredShading and DeferredReflectionCustom shaders in the Addressable Graphics settings and by proxy in the Project's Graphics Settings";
         public override string CustomDeferredReflection => "RoR2/Base/Shaders/Internal-DeferredReflections.shader";
         public override string CustomDeferredShading => "RoR2/Base/Shaders/Internal-DeferredShadingCustom.shader";
+        public override string CustomDeferredScreenspaceShadows => "NGSS/NGSS_Directional.shader";
     }
 }
