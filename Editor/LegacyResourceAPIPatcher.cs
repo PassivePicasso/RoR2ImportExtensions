e19454a38d3090ba127315cedfa62d24ba2bbc5f
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-05-18 13:20:24 -0400

    update to use managed assembly patcher

--- Editor/LegacyResourceAPIPatcher.cs
+++ Editor/LegacyResourceAPIPatcher.cs
@@ -1,45 +1,15 @@
 using System.IO;
 using ThunderKit.Core.Config;
-using ThunderKit.Core.Data;
-using ThunderKit.Core.Utilities;
-using UnityEditor;
 
 
 namespace RiskOfThunder.RoR2Importer
 {
-    public class LegacyResourceAPIPatcher : OptionalExecutor
+    public class LegacyResourceAPIPatcher : ManagedAssemblyPatcher
     {
         public override string Name => "RoR2 LegacyResourceAPI Patcher";
-
         public override string Description => $"Patches the game's LegacyResourcesAPI.dll to improve stability and reduce editor hangs.";
         public override int Priority => Constants.Priority.LegacyResourceAPIPatcher;
-
-        public override bool Execute()
-        {
-            var settings = ThunderKitSetting.GetOrCreateSettings<ThunderKitSettings>();
-            var legacyResourceAPIOrig = Path.GetFullPath(Path.Combine(settings.ManagedAssembliesPath, "LegacyResourcesAPI.dll"));
-            var legacyResourceAPINew = Path.GetFullPath(Path.Combine(settings.PackageFilePath, "LegacyResourcesAPI.dll"));
-            var diffPath = Path.GetFullPath(Path.Combine("Packages", "riskofthunder-ror2importer", "BinaryDiff", "LegacyResourcesAPI.diff"));
-            var packagePath = Path.GetDirectoryName(legacyResourceAPINew);
-
-            if (File.Exists(legacyResourceAPINew))
-                File.Delete(legacyResourceAPINew);
-
-            Directory.CreateDirectory(packagePath);
-
-            BsDiff.BsTool.Patch(legacyResourceAPIOrig, legacyResourceAPINew, diffPath);
-
-            var asmPath = legacyResourceAPINew.Replace("\\", "/");
-            string assemblyFileName = Path.GetFileName(asmPath);
-            var destinationMetaData = Path.Combine(settings.PackageFilePath, $"{assemblyFileName}.meta");
-            PackageHelper.WriteAssemblyMetaData(asmPath, destinationMetaData);
-
-            var escape = false;
-            while (EditorApplication.isUpdating && !escape)
-            {
-                var x = escape;
-            }
-            return true;
-        }
+        public override string AssemblyFileName => "LegacyResourcesAPI.dll";
+        public override string BsDiffPatchPath => Path.Combine("Packages", "riskofthunder-ror2importer", "BinaryDiff", "LegacyResourcesAPI.diff");
     }
 }
