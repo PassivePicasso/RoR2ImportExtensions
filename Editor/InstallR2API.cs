dda19b4573f33b10c52258a7a07c4971783fba21
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-05-18 18:52:23 -0400

    Update to use new TPI design

--- Editor/InstallR2API.cs
+++ Editor/InstallR2API.cs
@@ -5,7 +5,7 @@ namespace RiskOfThunder.RoR2Importer
     public class InstallR2API : ThunderstorePackageInstaller
     {
         public override int Priority => Constants.Priority.InstallR2API;
-        public override string SourcePath => "Assets/ThunderKitSettings/RoR2Thunderstore.asset";
+        public override string ThunderstoreAddress => "https://thunderstore.io";
         public override string DependencyId => "tristanmcpherson-R2API";
         public override string Description => $"Installs the R2API, a community supported common API for modding Risk of Rain 2";
         public override string Name => $"Install R2API";
--- Editor/InstallRoR2EK.cs
+++ Editor/InstallRoR2EK.cs
@@ -5,7 +5,7 @@ namespace RiskOfThunder.RoR2Importer
     public class InstallRoR2EK : ThunderstorePackageInstaller
     {
         public override int Priority => Constants.Priority.InstallRoR2EK;
-        public override string SourcePath => "Assets/ThunderKitSettings/ThunderKit Extensions.asset";
+        public override string ThunderstoreAddress => "https://thunderkit.thunderstore.io";
         public override string DependencyId => "RiskofThunder-RoR2EditorKit";
         public override string Description => $"Installs the RoR2 Editor Kit Package from the ThunderKit Extension store";
         public override string Name => $"Install RoR2 Editor Kit";
